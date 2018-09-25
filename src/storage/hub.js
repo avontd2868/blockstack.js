d2e615d9f2cb3202bc4096a0094ef86cdcf3ff37
Hank Stoever <hstove@gmail.com>
2018-09-24 19:52:14 -0700

    eslint fixes

--- src/storage/hub.js
+++ src/storage/hub.js
@@ -70,7 +70,10 @@ function makeLegacyAuthToken(challengeText: string, signerKeyHex: string): strin
   }
 }
 
-function makeV1GaiaAuthToken(hubInfo: Object, signerKeyHex: string, hubUrl: string, scopes?: Array<AuthScopeType>): string {
+function makeV1GaiaAuthToken(hubInfo: Object, 
+                             signerKeyHex: string, 
+                             hubUrl: string, 
+                             scopes?: Array<AuthScopeType>): string {
   const challengeText = hubInfo.challenge_text
   const handlesV1Auth = (hubInfo.latest_auth_version
                          && parseInt(hubInfo.latest_auth_version.slice(1), 10) >= 1)
